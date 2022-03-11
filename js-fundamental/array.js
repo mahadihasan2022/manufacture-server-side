b410b02771f5d7c2ba7f3c2ff03ae31f586fbd4d
mahadihasan2022 <mahadihasanmim72@gmail.com>
2022-03-11 12:39:51 +0600

    first commit

---
+++ js-fundamental/array.js
@@ -0,0 +1,3 @@
+var friendages = [21, 23, 11, 13, 15, 17, 19, 22];
+var gf = ['moriom', 'sadia','sunny',];
+console.log(gf);
---
+++ js-fundamental/variable.js
@@ -0,0 +1,7 @@
+// number 
+var watchprice = 120;
+watchprice = 124;
+// string
+var username = 'sumon';
+// Boolean
+var isexpensive = true;
